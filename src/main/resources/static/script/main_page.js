d2f7e564f43a9d5d99e9e2a31b4afaac288dea62
IcebearDL <u201616984@hust.edu.cn>
2019-06-22 11:24:05 +0800

    js优化

--- src/main/resources/static/script/main_page.js
+++ src/main/resources/static/script/main_page.js
@@ -1,4 +1,4 @@
-const baseURL = "http://localhost:8080";
+const baseURL = "http://localhost:8080/zhifou";
 
 function getInformation(type) { 
     //放在外层用于“查看更多的复用”
@@ -68,62 +68,57 @@ function getInformation(type) {
             `;
         });
 
-        let insertDiv = document.getElementById("pageCardMore");
-
+        //将cardColumns插入到useToInsert这个div之前
+        let insertDiv = document.querySelector(".useToInsert");
         let cardColumns = document.createElement("div");
         cardColumns.className = "page-cards-container";
         cardColumns.innerHTML = htmlQuestionsString;
-        //将cardColumns插入到insertDiv这个div之前
         insertDiv.parentNode.insertBefore(cardColumns,insertDiv);
     }
 
     if (type === "firstLoad"){
 
-        fetch(baseURL+'/answer').then(response =>{
+        fetch(baseURL+'/answer/extraction').then(response =>{
             if(response.ok){
                 console.log("请求问题列表成功！");
                 return response.json();
             }
         }).then( res=>{
 
+            //将左侧移除
+            let cards = document.querySelectorAll(".page-cards-container");
+            if (cards.length !== 0){
+                for (let i =0; i<cards.length ; i++){
+                    cards[i].remove();
+                }
+                // displayThis("block","pageCardMore");
+            }
+            console.log("移除成功");
+
             let questionList;
             questionList = res.data;
             //对显示数量及数据是否为空进行判断限制
             displayQuestionsToHTML(questionList);
 
-        }).then(function () {
+            //将查看更多推荐插入最后一个card前面
+            let insertDiv = document.querySelector(".useToInsert");
+            let readMore = document.createElement("div");
+            readMore.className = "page-cards-container";
+            let htmlReadMoreHTML;
+            htmlReadMoreHTML = `
+                <div class="page-card-tips" onclick="getInformation('moreLoad')" style="text-align: center;color: #8590a6;cursor: pointer" id="pageCardMore">
+                        <div class="page-card-more">
+                            <span>查看更多推荐</span>
+                            <i class="fa fa-angle-down" aria-hidden="true"></i>
+                     </div>
+            </div>
+            `;
+            readMore.innerHTML = htmlReadMoreHTML;
+            insertDiv.parentNode.insertBefore(readMore,insertDiv.nextSibling);
+            console.log("插入更多推荐成功");
 
-            displayThis("block","pageRightPart");
-            displayThis("block","pageCardMore");
-
-        }).catch(function(e){
-            alert("error:" + e);
-        });
-
-    } else if (type === "secondLoad"){
-
-        fetch(baseURL+'/answer').then(response =>{
-            if(response.ok){
-                console.log("请求问题列表成功！");
-                return response.json();
-            }
-        }).then( res=>{
-
-            displayThis("block","pageCardMore");
-            let cards = document.getElementsByClassName("page-cards-container");
-            //点击“我的关注” 移除所有推荐问题
-            for (let i =0; i<cards.length ; i++){
-                cards[i].remove();
-            }
-            return res;
-
-        }).then(res =>{
-
-            let questionList;
-            questionList = res.data;
-            //对显示数量及数据是否为空进行判断限制
-            displayQuestionsToHTML(questionList);
-            displayThis("block","pageCardMore");
+            //cards插入完成后，显示右侧栏
+            document.getElementById("pageRightPart").style.display = "block";
 
         }).catch(function(e){
             alert("error:" + e);
@@ -131,7 +126,7 @@ function getInformation(type) {
 
     } else if (type === "moreLoad"){
         //点击阅读更多之后
-        fetch(baseURL+'/answer').then(response =>{
+        fetch(baseURL+'/answer/extraction').then(response =>{
             if(response.ok){
                 console.log("请求问题列表成功！");
                 return response.json();
@@ -234,22 +229,42 @@ function cardOperation(type,id) {
 function navOperation(type,id) {
     if(type === "displayMyFocus"){
 
-        let cards = document.getElementsByClassName("page-cards-container");
         //点击“我的关注” 移除所有推荐问题
-        if(cards !== null){
-            for (let i =0; i<cards.length ; i++){
+        let cards = document.querySelectorAll(".page-cards-container");
+        if(cards.length !== 0){
+            for (let i =0; i<cards.length; i++){
                 cards[i].remove();
             }
         }
-        //移除“更多推荐”
-        let cardMore = document.getElementById("pageCardMore");
-        if(cardMore !== null) cardMore.remove();
 
-        //判断如果没有focus则显示
-        displayThis("block","noFocusTip")
+        //判断如果没有关注则插入
+        if(true){
+            let insertDiv = document.querySelector(".useToInsert");
+            let noFocusTip = document.createElement("div");
+            noFocusTip.className = "page-cards-container";
+            let htmlnoFocusTipHTML;
+            htmlnoFocusTipHTML = `
+                <div class="page-card-tips" style="text-align: center;color: #8590a6" id="noFocusTip">
+                    <div class="page-card-more">
+                        <span>您暂时还没有关注，快关注以下推荐内容吧！</span>
+                    </div>
+                </div>
+            `;
+            noFocusTip.innerHTML = htmlnoFocusTipHTML;
+            insertDiv.parentNode.insertBefore(noFocusTip,insertDiv.nextSibling);
+            console.log("插入成功");
+        }
+
+
 
     } else if(type === "displayNewQuestions"){
-
+        //点击“最新提问” 移除所有其他
+        let cards = document.querySelectorAll(".page-cards-container");
+        if(cards.length !== 0){
+            for (let i =0; i<cards.length; i++){
+                cards[i].remove();
+            }
+        }
     }
 }
 
